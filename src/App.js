61e4fd1c4685a93d03dcb73d0b0ade4569602fa8
chandru90 <iochandru90@gmail.com>
2023-02-28 14:01:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import ExpenseItem from "./components/ExpenseItem";
 const expense =[
-  { amount : 200 , title:"food"},
-  { amount : 300 , title:"movie"},
-  { amount : 400 , title:"petrol"},
-  { amount : 200 , title:"grocery"}
+  { amount : 200 , title:"food" ,location : "pondy"},
+  { amount : 300 , title:"movie",location :"pondy"},
+  { amount : 400 , title:"petrol",location :"pondy"},
+  { amount : 200 , title:"grocery",location :"pondy"}
 
 ]
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title ={expense[0].title } amount={expense[0].amount} ></ExpenseItem>
-      <ExpenseItem title ={expense[1].title } amount={expense[1].amount} ></ExpenseItem>
-      <ExpenseItem title ={expense[2].title } amount={expense[2].amount} ></ExpenseItem>
-      <ExpenseItem title ={expense[3].title } amount={expense[3].amount}></ExpenseItem>
+      <ExpenseItem title ={expense[0].title } amount={expense[0].amount} location={expense[0].location}></ExpenseItem>
+      <ExpenseItem title ={expense[1].title } amount={expense[1].amount} location={expense[1].location}></ExpenseItem>
+      <ExpenseItem title ={expense[2].title } amount={expense[2].amount} location={expense[2].location}></ExpenseItem>
+      <ExpenseItem title ={expense[3].title } amount={expense[3].amount}location={expense[3].location}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,6 +9,7 @@ function ExpenseItem(props)
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
         <div className='expense-item__price'>Rs.{props.amount}</div>
+        <div className='expense-item__location'>{props.location}</div>
         </div>
     </div>
 }
